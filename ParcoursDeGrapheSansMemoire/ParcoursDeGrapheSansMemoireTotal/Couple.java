2d3778a1bccc838aea3fed7d9a687c0964b8a4bb
clairelsr <34139274+clairelsr@users.noreply.github.com>
2017-11-30 20:34:01 +0100

    Add files via upload

---
+++ ParcoursDeGrapheSansMemoire/ParcoursDeGrapheSansMemoireTotal/Couple.java
@@ -0,0 +1,13 @@
+package ParcoursDeGrapheSansMemoireTotal;
+
+class Couple
+{
+    public Boolean b;
+    public int i;
+
+    public Couple(Boolean b, int i)
+    {
+         this.b = b;
+         this.i = i;
+    }
+}
